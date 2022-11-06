3b0376a319c6d0d528dee4a1448a710d00a09724
Alexandr Sirotkin <aasirotkin@me.com>
2022-11-06 12:21:58 +0300

    Create button counter with label

--- SimpleCounter/ViewController.swift
+++ SimpleCounter/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+
+    @IBOutlet weak var counterButton: UIButton!
+
+    private let counterLabelBaseText = "Значение счётчика: "
+    private let counterButtonText = "Нажми меня"
+    private var counter = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        counterLabel.text = getCounterLabelText()
+        counterButton.setTitle(counterButtonText, for: .normal)
     }
 
+    private func getCounterLabelText() -> String {
+        return counterLabelBaseText + String(counter)
+    }
+
+    @IBAction func counterButtonClicked() {
+        counter += 1
+        counterLabel.text = getCounterLabelText()
+    }
 
 }
-
